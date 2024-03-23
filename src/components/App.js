79916d7ddd117117672aee92df7634765360bed9
Mehdi Şahin <sahinmehdi23@gmail.com>
2024-03-23 23:40:18 +0300

    add lazy loading

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,22 @@
-import DetailPage from "./DetailPage";
-import HomePage from "./HomePage";
+import react, { Suspense } from "react";
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+// Add Lazy Loading //
+const DetailPage = react.lazy(() => import("./DetailPage"));
+const HomePage = react.lazy(() => import("./HomePage"));
+
 function App() {
-	return (
-		<div>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<HomePage />}></Route>
-					<Route
-						path="/weather-details/:cityName"
-						element={<DetailPage />}
-					></Route>
-				</Routes>
-			</BrowserRouter>
-		</div>
-	);
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/weather-details/:cityName" element={<DetailPage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
+  );
 }
 
 export default App;
